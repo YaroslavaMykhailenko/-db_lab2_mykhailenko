3d86c8670082b5d8f776e8e7bae37fd029f339d3
YaroslavaMykhailenko <55324111+YaroslavaMykhailenko@users.noreply.github.com>
2021-11-03 17:11:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import psycopg2
+import math
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+username = 'postgres'
+password = 'Slava270602'
+database = 'mykhailenkoyaroslava'
+host = 'localhost'
+port = '5432'
 
+query_1 = '''
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+select order_date, count(*) as quantity_orders from orders group by order_date
+'''
+query_2 = '''
+select TRIM(kitchen_type), count(*) as count_cuisine from restaurants group by kitchen_type
+'''
+query_3 = '''
+SELECT COUNT(prod_price) AS quantity_products, products.prod_price
+FROM products JOIN orders USING(prod_id)  GROUP BY prod_price
+ORDER BY prod_price
 
+'''
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+con = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
+print(type(con))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with con:
+
+    cur = con.cursor()
+
+    print('1.  \n')
+    cur.execute(query_1)
+    for row in cur:
+        print(row)
+
+    print('\n2.  \n')
+    cur.execute(query_2)
+    for row in cur:
+        print(row)
+
+    print('\n3.  \n')
+    cur.execute(query_3)
+    for row in cur:
+        print(row)
