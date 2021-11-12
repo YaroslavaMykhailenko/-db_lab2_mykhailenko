2694f874115ea151d388ae4b78c05088de1b9eec
YaroslavaMykhailenko <55324111+YaroslavaMykhailenko@users.noreply.github.com>
2021-11-12 23:03:51 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -10,15 +10,19 @@ port = '5432'
 
 query_1 = '''
 
-select order_date, count(*) as quantity_orders from orders group by order_date
+SELECT order_date, COUNT(*) AS quantity_orders
+FROM orders
+GROUP BY order_date
 '''
 query_2 = '''
-select TRIM(kitchen_type), count(*) as count_cuisine from restaurants group by kitchen_type
+SELECT TRIM(restaurants.rest_name), COUNT(rest_id)
+FROM restaurants JOIN orders USING(rest_id)
+GROUP BY rest_id 
 '''
 query_3 = '''
-SELECT COUNT(prod_price) AS quantity_products, products.prod_price
-FROM products JOIN orders USING(prod_id)  GROUP BY prod_price
-ORDER BY prod_price
+SELECT prod_price, sum(quantity) AS quantity_products
+FROM products JOIN orderitems USING(prod_id)
+GROUP BY prod_price ORDER BY prod_price
 
 '''
 
@@ -53,11 +57,11 @@ with con:
         print(row)
 
     fig, ax = plt.subplots()
-    colors = ['#4F6272', '#B7C3F3', '#DD7596', '#8EB897']
+    colors = ['#4F6272', '#B7C3F3', '#DD7596', '#8EB897','#B7C3F3']
     ax.pie(diagram_pie.values(), labels = diagram_pie.keys(), autopct='%1.1f%%', colors=colors)
-    plt.title('Поділ ринку між різними національностями кухні')
+    plt.title('Скільки відсотків ринку належить кожному ресторану')
     plt.show()
-
+    
     print('3.  \n')
 
     cur.execute(query_3)
@@ -65,8 +69,8 @@ with con:
     price = []
     quantity = []
     for row in cur:
-        price.append(row[1])
-        quantity.append(row[0])
+        price.append(row[0])
+        quantity.append(row[1])
 
         print(row)
 
